c3f5cf45835176e59ce47141ddcb2a035e9d48f5
ghiaog123 <vuminhhieu2412@gmail.com>
2020-07-30 17:05:54 +0700

    second commit

--- main.py
+++ main.py
@@ -1,50 +1,56 @@
-file_input = open('data.txt', 'r')
-lines = file_input.readlines()
 
-userFriendList = {}
-sumUser = sum(1 for _ in file_input)
-for line in lines:
-    user,friendListString = line.split('\t')
-    user = int(user)
-    if len(friendListString) != 1: # bo nhung id nao ko co ban
-        removeddownline, downline = friendListString.split('\n') #tach ki tu xuong dong \n
-        friends = removeddownline.split(',') #bo dau ,   
-        friendList = []
-        for i in friends:
-            friendList.append(i)
-        if friendList:
-            userFriendList[user] = friendList #tao mot dic chua friend
+def read_input ( lines ):
+    user_friend_lists = {}
+    for line in lines:
+        user,friendListString = line.split('\t')
+        user = int(user)
+        if len(friendListString) != 1: # bo nhung id nao ko co ban
+            user_friend_lists[user] = [int(i) for i in friendListString.strip().split(',')]
+    return user_friend_lists
+
+def find_recomment_friend( user_friend_list, U):
+    recomment_friend = {}
+    for i in user_friend_list:
+        if( i != U):
+            set_user = set(user_friend_list[U]) 
+            set_other_user = set(user_friend_list[i])
+            mutualFriend = set_user.intersection(set_other_user) #tim so ban giong nhau
+            if len(recomment_friend) < N : #neu hang cho chua day
+                recomment_friend[i] = len(mutualFriend)
+            else:
+                minval = min(recomment_friend.values()) #tim value nho nhat trong hang cho
+                if len(mutualFriend) >= minval :
+                    res = [k for k, v in recomment_friend.items() if v==minval] #tim list key co value be nhat
+                    res.sort()
+                    recomment_friend[i] = len(mutualFriend) 
+                    del recomment_friend[res.pop()] #xoa phan tu 
+    return recomment_friend
+
+def result( U , user_friend_list):
+    output = open('output.txt','w')
+    if U not in user_friend_list and U < 50000: # neu ko co ban hoac ko ton tai
+        output.write("Not have a friend ")
+    elif U >= 50000:
+        output.write("Don't have a id? Do you want to create an account?")
+    else: 
+        recomment_friend = find_recomment_friend(user_friend_list, U) #tao hang cho
         
+        rcmtFriendList = [str(interger) for interger in recomment_friend.keys()]
+        string_rcmtFriendList = "\n".join(rcmtFriendList)
+        output.write(string_rcmtFriendList)
 
+file_input = open('data.txt', 'r')
+
+lines = file_input.readlines()
+user_friend_list = read_input(lines)
+   
 file_input.close()
 
 U = int(input())
 N = int(input())
 
-recommentFriend = {} #tao hang cho
+result(U, user_friend_list)
 
-output = open('output.txt','w')
-if U not in userFriendList: # neu ko co ban hoac ko ton tai
-    output.write("Not have a friend or this id is not exist")
-else: 
-    for i in userFriendList:
-        if( i != U):
-            setUser = set(userFriendList[U]) 
-            setOtherUser = set(userFriendList[i])
-            mutualFriend = setUser.intersection(setOtherUser) #tim so ban giong nhau
-            if len(recommentFriend) < N : #neu hang cho chua day
-                recommentFriend[i] = len(mutualFriend)
-            else:
-                minval = min(recommentFriend.values()) #tim value nho nhat trong hang cho
-                if len(mutualFriend) >= minval :
-                    res = [k for k, v in recommentFriend.items() if v==minval] #tim list key co value be nhat
-                    res.sort()
-                    recommentFriend[i] = len(mutualFriend) 
-                    del recommentFriend[res.pop()] #xoa phan tu 
-    
-    rcmtFriendList = [str(interger) for interger in recommentFriend.keys()]
-    string_rcmtFriendList = "\n".join(rcmtFriendList)
-    output.write(string_rcmtFriendList)
     
 
 

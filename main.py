dcf8cc240929a50b2991fc85ff06f341c932d297
ghiaog123 <vuminhhieu2412@gmail.com>
2020-08-14 23:09:59 +0700

    Another exercise

--- main.py
+++ main.py
@@ -1,83 +1,62 @@
 
-def read_input ( lines ):
-    user_friend_lists = {}
-    for line in lines:
-        user,friendListString = line.split('\t')
-        user = int(user)
-        if len(friendListString) != 1: # bo nhung id nao ko co ban
-            user_friend_lists[user] = [int(i) for i in friendListString.strip().split(',')]
-    return user_friend_lists
+def read_input(file_name):
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        return [line.split() for line in lines]   
 
-
-def find_recomment_friend( user_friend_list, U, N):
-    
-    recomment_friend = {}
-    for i in user_friend_list:
-        
-        if( i != U):
-            set_user = set(user_friend_list[U]) 
-            set_other_user = set(user_friend_list[i])
-            mutualFriend = set_user.intersection(set_other_user) #find mutual friend
-            
-            if len(recomment_friend) < N : 
-                recomment_friend[i] = len(mutualFriend)
-                
+def find_count_items(transactions):
+    count_items = {}
+    for basket in transactions:
+        for i in basket:
+            if i in count_items.keys():
+                count_items[i] += 1
             else:
-                minval = min(recomment_friend.values()) #find min value
-                
-                if len(mutualFriend) >= minval :
-                    res = [k for k, v in recomment_friend.items() if v==minval] 
-                    res.sort()
-                    recomment_friend[i] = len(mutualFriend) 
-                    del recomment_friend[res.pop()] #delete the highest key
+                count_items[i] = 1
+    return count_items
     
-    return sort_the_result(recomment_friend, N)
- 
- 
-def sort_the_result( recomment_friend , N ): #sort the recomment friend
-    
-    if len(recomment_friend) < N:
-        recomment_friend_sorted =sorted(recomment_friend.items(), key=lambda x : x[1], reverse = True)
-        recomment_friend_sorted = dict(recomment_friend_sorted)
-        return recomment_friend_sorted
-    
-    else:
-        recomment_friend_sorted = sorted(recomment_friend.items(), reverse = False)
-        recomment_friend_sorted = dict(recomment_friend_sorted)
-        return recomment_friend_sorted
-  
-def result( U , user_friend_list, N):
-    output = open('output.txt','w')
-    
-    if U not in user_friend_list and U < 50000: # if id not exist
-        str_U = str(U)
-        output.write(str(U))
-        
-    elif U >= 50000:
-        output.write("Don't have a id? Do you want to create an account?")
-        
-    else: 
-        recomment_friend = find_recomment_friend(user_friend_list, U, N) 
-        str_U = str(U) + '\t'
-        output.write(str_U)
-        
-        rcmtFriendList = [str(interger) for interger in recomment_friend.keys()]
-        string_rcmtFriendList = ", ".join(rcmtFriendList)
-        output.write(string_rcmtFriendList)
-
-
-file_input = open('data.txt', 'r')
-
-lines = file_input.readlines()
-user_friend_list = read_input(lines)
+def find_frequent_items(count_items, s):
+    #frequent_items = dict([(k, v) for k, v in count_items.values() if v >= s])  ko chay duoc
+    frequent_items = {}
+    for i in count_items.keys():
+        if count_items[i] >= s:
+            frequent_items[i] = count_items[i]   
+    return frequent_items
    
-file_input.close()
-
-U = int(input())
-N = int(input())
-
-result(U, user_friend_list, N)
-
+def find_baskets_frequent_item_appear(frequent_items, baskets):
     
+    baskets_frequent_item_appear = {}
+    numb_basket = 0
+    for basket in baskets:
+        numb_basket += 1
+        for i in basket:
+            if i in frequent_items.keys():
+                if i not in baskets_frequent_item_appear:
+                    baskets_frequent_item_appear[i] = []
+                    baskets_frequent_item_appear[i].append(numb_basket)
+                else:
+                    baskets_frequent_item_appear[i].append(numb_basket)
+                    
+    return baskets_frequent_item_appear
 
-
+def find_association_rules(baskets_frequent_items_appear, s):
+    same_basket = []
+    for i in baskets_frequent_items_appear.keys():
+        for j in baskets_frequent_items_appear.keys():
+            if i < j:
+                d = [item for item in baskets_frequent_items_appear[i] if item in baskets_frequent_items_appear[j]]
+                if len(d) >= s :
+                    same_x_y = [i,j,len(d)]
+                    same_basket.append(same_x_y)
+    return same_basket
+    
+if __name__ == '__main__':
+    inputss = [['a','b'],['a','c','d'], ['a','b']]
+    s = 100
+    
+    inputs = read_input('browsing.txt')
+    count_items = find_count_items(inputs)
+    frequent_items = find_frequent_items(count_items, s)
+    baskets_frequent_items_appear = find_baskets_frequent_item_appear(frequent_items, inputs)
+    same_basket = find_association_rules(baskets_frequent_items_appear, s)
+    print(same_basket.pop())
+    
